afc4634b7b3421bc7cabfc56bab48922138c66d3
Elna Halilovic <elna.halilovic@diamir.io>
2023-11-04 18:18:07 +0100

    start game by clicking on Start button in title screen

---
+++ Assets/TitleMenu.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ShowManual()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void ShowSettings()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
