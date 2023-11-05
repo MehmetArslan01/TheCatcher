b4db6ae8a8254b0695fd9362aa4cb5ff44d75408
Elna Halilovic <elna.halilovic@diamir.io>
2023-11-05 16:44:55 +0100

    add about pages

---
+++ Assets/PageController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PageController : MonoBehaviour
+{
+
+    public CanvasGroup page1;
+    public CanvasGroup page2;
+
+    public void getNextPage()
+    {
+        page1.alpha = 0.0f;
+        page2.alpha = 1.0f;
+    }
+
+    public void getPreviousPage()
+    {
+        page1.alpha = 1.0f;
+        page2.alpha = 0.0f;
+    }
+
+}
