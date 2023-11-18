64cd1c387f29dafe5e695a0287a728170e8ca1e5
Elna Halilovic <elna.halilovic@diamir.io>
2023-11-18 13:48:10 +0100

    add timer

---
+++ Assets/HUDController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class HUDController : MonoBehaviour
+{
+    public TextMeshProUGUI time;
+    public float matchTimeInMin;
+
+    private float matchTimeInSec;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.matchTimeInSec = (this.matchTimeInMin * 60);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.matchTimeInSec -= Time.deltaTime;
+        var ts = TimeSpan.FromSeconds(this.matchTimeInSec);
+        this.time.text = string.Format("{0:00}:{1:00}", ts.Minutes, ts.Seconds);
+
+        if (this.matchTimeInSec <= 0.0f)
+        {
+            timerFinished();
+        }
+    }
+
+    void timerFinished()
+    {
+        //TODO: do something after timer finished
+    }
+}
